0d046a90b44008cfcce481e59af89bda135ee32b
Yaroslav Sorochan <yaroslavsorocan89@gmail.com>
2026-02-05 18:35:51 +0200

    Use the same variables as in development

--- config/config.js
+++ config/config.js
@@ -17,8 +17,16 @@ module.exports = {
     dialect: 'mysql'
   },
   production: {
-    use_env_variable: 'DATABASE_URL',
-    dialect: 'mysql'
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 3306,
+    dialect: 'mysql',
+    dialectOptions: {
+      ssl: {
+        rejectUnauthorized: true
+      }
+    }
   }
 };
- 
