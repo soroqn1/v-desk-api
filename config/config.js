a4a8a63ae778b687b55b48d09fb2abd08d52b07a
Yaroslav Sorochan <yaroslavsorocan89@gmail.com>
2026-02-05 21:36:24 +0200

    simplify production database configuration to use DATABASE_URL

--- config/config.js
+++ config/config.js
@@ -17,16 +17,11 @@ module.exports = {
     dialect: 'mysql'
   },
   production: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    port: parseInt(process.env.DB_PORT || '3306', 10),
+    use_env_variable: 'DATABASE_URL',
     dialect: 'mysql',
     dialectOptions: {
       connectTimeout: 60000,
       ssl: {
-        require: true,
         rejectUnauthorized: false
       }
     }
